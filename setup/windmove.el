86a8a14b66b27727a503df09b7e0ca1198431660
Stefan Bund <stefan@j32.de>
2013-05-07 13:05:47 +0200

    alternative keybindings for text mode emacs

--- setup/windmove.el
+++ setup/windmove.el
@@ -1,5 +1,12 @@
 (require 'windmove)
-(global-set-key (kbd "S-<left>") 'windmove-left)
-(global-set-key (kbd "S-<right>") 'windmove-right)
-(global-set-key (kbd "S-<up>") 'windmove-up)
-(global-set-key (kbd "S-<down>") 'windmove-down)
+
+(if window-system
+    (progn
+      (global-set-key (kbd "S-<left>") 'windmove-left)
+      (global-set-key (kbd "S-<right>") 'windmove-right)
+      (global-set-key (kbd "S-<up>") 'windmove-up)
+      (global-set-key (kbd "S-<down>") 'windmove-down))
+  (global-set-key (kbd "C-c <left>") 'windmove-left)
+  (global-set-key (kbd "C-c <right>") 'windmove-right)
+  (global-set-key (kbd "C-c <up>") 'windmove-up)
+  (global-set-key (kbd "C-c <down>") 'windmove-down))
