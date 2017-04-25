bcb103dbc0aa89057d8d0b549f5d4cdb841a6795
Stefan Bund <stefan.bund@traveltainment.de>
2017-04-25 18:10:15 +0200

    implement eshell-switch-directory-current-buffer bound to \C-cE

--- setup/eshell.el
+++ setup/eshell.el
@@ -9,3 +9,17 @@
   (define-key eshell-mode-map (kbd "<home>") 'eshell-bol))
 
 (add-hook 'eshell-mode-hook 'my-setup-eshell)
+
+(defun eshell-switch-directory-current-buffer ()
+  (interactive)
+  (let ((dir default-directory)
+        (eshell-buffer (get-buffer "*eshell*")))
+    (if (not eshell-buffer)
+        (error "no *eshell* buffer found"))
+    (pop-to-buffer (get-buffer "*eshell*"))
+    (goto-char (point-max))
+    (setq default-directory dir)
+    (insert "\n")
+    (eshell-send-input)))
+
+(global-set-key "\C-cE" 'eshell-switch-directory-current-buffer)
